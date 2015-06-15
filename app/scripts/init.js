d91e9fb3e8d21d26d3ea81aae77168fcf144f487
kiki le singe <aalbertini95@gmail.com>
2015-06-15 11:20:30 +0200

    feat(init): initialize application

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,4 +1,16 @@
-// import App from './App';
-// import Backbone from 'backbone';
+// https://babeljs.io/docs/learn-es2015/
+// https://github.com/addyosmani/todomvc-backbone-es6
+// http://www.2ality.com/2014/09/es6-modules-final.html
 
-document.write('ooiuuyy');
+import $ from 'jquery';
+import Backbone from 'backbone';
+import Routers from './routers/routers';
+
+$(() => {
+  new Routers();
+  Backbone.history.start();
+
+  console.log($);
+  console.log(Backbone);
+  console.log(Routers);
+});
