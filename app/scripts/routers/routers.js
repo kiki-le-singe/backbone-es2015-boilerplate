de0ee2dea0a9a38a0c369101ca226ec518cdc2ae
kiki le singe <aalbertini95@gmail.com>
2015-06-15 11:20:46 +0200

    feat(routers): add app routers

---
+++ app/scripts/routers/routers.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import {Router} from 'backbone';
+import indexView from '../views/index';
+
+export default class extends Router {
+
+  constructor() {
+    super({
+      routes: {
+        '': 'index'
+      }
+    });
+    console.log('indexRouter');
+  }
+
+  index () {
+    $('#content').html(new indexView().render().el);
+    console.log('index');
+  }
+
+  bar (options) {
+  }
+}
