9bf05a71990ea5b46c7f0100e95afd6cb9f42f4e
kiki le singe <aalbertini95@gmail.com>
2015-06-16 23:09:45 +0200

    docs(init): add resource

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,6 +1,7 @@
 // https://babeljs.io/docs/learn-es2015/
 // https://github.com/addyosmani/todomvc-backbone-es6
 // http://www.2ality.com/2014/09/es6-modules-final.html
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Instructions/import
 
 import $ from 'jquery';
 import Backbone from 'backbone';
